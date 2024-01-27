94b5866abcd618b43c3b9ef94ab95ca5dffc1dac
Athul Raj V <athulrajv13@gmail.com>
2024-01-27 23:57:50 +0530

    Completed Hero section and added About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,24 @@
 'use client';
+import { motion     } from "framer-motion";
+import { TypingText } from "../components";
+import { fadeIn, staggerContainer } from '../utils/motion';
+import   styles       from '../styles';
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{once:false, amount:0.25}}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText  
+        title="| About Metaversus"
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
